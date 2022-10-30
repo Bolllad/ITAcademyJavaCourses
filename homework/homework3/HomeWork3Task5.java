680bbf750760d62bbae645c48f9b1349de0ed991
Bolllad <bollladmail@gmail.com>
2022-10-31 01:05:19 +0300

    HomeWork4

--- homework/homework3/HomeWork3Task5.java
+++ homework/homework3/HomeWork3Task5.java
@@ -1,3 +1,5 @@
+package homework3;
+
 import java.util.Scanner;
 
 public class HomeWork3Task5 {
---
+++ homework/homework4/HomeWork4Task1.java
@@ -0,0 +1,14 @@
+package homework4;
+
+public class HomeWork4Task1 {
+
+    public static void main(final String[] args) {
+        int num = 5, result;
+
+        for (int i = 1; i <= 10; i++) {
+            result = num * i;
+            System.out.println(num + " * " + i + " = " + result);
+        }
+    }
+
+}
