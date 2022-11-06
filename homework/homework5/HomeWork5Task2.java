ea0b2417753678167c70753e156be58de377c804
Bolllad <bollladmail@gmail.com>
2022-11-06 20:22:16 +0300

    HomeWork5Task2 update

--- homework/homework5/HomeWork5Task2.java
+++ homework/homework5/HomeWork5Task2.java
@@ -3,22 +3,36 @@ package homework5;
 public class HomeWork5Task2 {
 
     public static void main(final String[] args) {
-        int[] array = {3, 5, 0, 1, 1, 3, 5, 2, 2, 2, 4};
-        int[] cleanArray = new int[array.length];
+        int length = 1;
+        int[] array = {3, 5, 0, 1, 1, 3, 5, 2, 2, 2, 4}, longerArray = new int[length];
 
         for (int counter = 0; counter < array.length; counter++) {
             boolean isRepeating = false;
 
-            for (int innerCount = counter + 1; innerCount < array.length; innerCount++) {
-                if (array[counter] == array[innerCount]) {
+            for (int innerCount = 0; innerCount < longerArray.length - 1; innerCount++) {
+                if (array[counter] == longerArray[innerCount]) {
                     isRepeating = true;
                     break;
                 }
             }
 
             if (!isRepeating) {
-                System.out.print(array[counter] + " ");
+                longerArray[length - 1] = array[counter];
+                int[] savedArray = longerArray.clone();
+
+                if (counter < array.length - 1) {
+                    length++;
+                    longerArray = new int[length];
+                }
+
+                for (int arrayCounter = 0; arrayCounter < savedArray.length; arrayCounter++) {
+                    longerArray[arrayCounter] = savedArray[arrayCounter];
+                }
             }
+        }
+
+        for (int count = 0; count < longerArray.length; count++) {
+            System.out.print(longerArray[count] + " ");
         }
     }
 
